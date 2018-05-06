92fad23955c8708e06d3c50fda2df7533e853b9f
Stefan Walther <swr@qlik.com>
2018-05-06 02:42:40 +0200

    test: Config winster

--- test/mocha.conf.js
+++ test/mocha.conf.js
@@ -3,6 +3,9 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
 
+process.env.NODE_ENV = 'test';
+process.env.WINSTER_SUPRESS_LOGGING = 'true';
+
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
 
